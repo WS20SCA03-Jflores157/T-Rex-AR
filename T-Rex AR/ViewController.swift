d2049b718cf94926f17c5bb4f0f3efebb17c37c0
Jeffrey <Jeffrey@simpleyapp.com>
2020-06-20 08:01:11 -0400

    Dinosaur 3d Model

--- T-Rex AR/ViewController.swift
+++ T-Rex AR/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.autoenablesDefaultLighting = true
+        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -24,7 +26,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        
+        let dinoScene = SCNScene(named: "trex.dae")
+        
+        guard let dinoNode = dinoScene?.rootNode.childNode(withName: "T-Rex", recursively: true) else {
+            fatalError("T-Rex is not found")
+        }
+        
+        dinoNode.position = SCNVector3(0,0,-1.0)
+        scene.rootNode.addChildNode(dinoNode)
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -46,30 +57,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+ 
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
